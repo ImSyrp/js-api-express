f746e2c11e83afdfeb718d34af4794177fffb3f8
Dylan Rodriguez <dylansrperez@gmail.com>
2026-03-31 19:48:23 -0600

    Backend con express

---
+++ public/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/saludo', (req, res) => {
+    res.json({
+        mensaje: "¡Hola desde el backend de la Casimiro Sotelo!",
+        estudiante: "Dylan",
+        universidad: "UNCSM",
+        unidad: "Unidad II: Herramientas para el desarrollo Web"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
