ffe7d7acf4b62ca84b09d42303664e4b573456aa
Josue <Cincojosue025@gmail.com>
2026-04-26 21:59:31 -0600

    configuracion de middlewares (logger y procesamiento json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+app.use(express.json());
 require('dotenv').config();
 
 const express = require('express');
@@ -7,6 +8,27 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Registro de actividad en la consola
+
+app.use((req, res, next) => {
+    const tiempo = new Date().toISOString();
+    console.log(`[LOG] ${tiempo} - Método: ${req.method} - URL: ${req.url}`);
+    next();
+});
+
+
+
+const validarAcceso = (req, res, next) => {
+    const token = req.query.token;
+
+    if (token === 'admin123') {
+        next();
+    } else {
+        res.status(401).send('<h1>No autorizado</h1><p>Token inválido</p>');
+    }
+};
 
 app.get('/api/saludo', (req, res) => {
     res.json({
