85e135b4145cdf1ce163f0c870c5207fc04e740e
Josue <Cincojosue025@gmail.com>
2026-04-26 21:03:49 -0600

    inicio del servidor con el puerto elegido en el .env

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
